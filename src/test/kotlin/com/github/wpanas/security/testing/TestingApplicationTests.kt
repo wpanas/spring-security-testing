1bed2b0456d176211e6194f72fb7aeed3e8ad11e
wpanas <wpanas@users.noreply.github.com>
2020-01-18 11:39:29 +0100

    Start Spring Web application

---
+++ src/test/kotlin/com/github/wpanas/security/testing/TestingApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.github.wpanas.security.testing
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TestingApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
